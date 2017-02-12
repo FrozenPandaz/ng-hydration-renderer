6d1e00064f719f04a8ae63f87978db8b8289d0b1
FrozenPandaz <jasonjean1993@gmail.com>
2017-02-12 18:38:42 -0500

    Removed stuff that the current Renderer already has

--- src/hydration-renderer/hydration-renderer.ts
+++ src/hydration-renderer/hydration-renderer.ts
@@ -6,13 +6,6 @@ import { Injectable, Renderer, RenderComponentType } from '@angular/core';
 import { RenderDebugInfo } from '@angular/core/src/render/api';
 import { DomRootRenderer, DomRenderer, DomRootRenderer_ } from '@angular/platform-browser/src/dom/dom_renderer';
 
-export const NAMESPACE_URIS: {[ns: string]: string} = {
-	'xlink': 'http://www.w3.org/1999/xlink',
-	'svg': 'http://www.w3.org/2000/svg',
-	'xhtml': 'http://www.w3.org/1999/xhtml'
-};
-const NS_PREFIX_RE = /^:([^:]+):(.+)$/;
-
 @Injectable()
 export class HydrationRootRenderer extends DomRootRenderer_ {
 
@@ -57,40 +50,6 @@ export class HydrationRenderer extends DomRenderer {
 
 		return super.createElement(parent, name, debugInfo);
 	}
-
-	setElementAttribute(renderElement: Element, attributeName: string, attributeValue: string): void {
-		let attrNs: string;
-		let attrNameWithoutNs = attributeName;
-		if (isNamespaced(attributeName)) {
-			const nsAndName = splitNamespace(attributeName);
-			attrNameWithoutNs = nsAndName[1];
-			attributeName = nsAndName[0] + ':' + nsAndName[1];
-			attrNs = NAMESPACE_URIS[nsAndName[0]];
-		}
-		if (isPresent(attributeValue)) {
-			if (attrNs) {
-				renderElement.setAttributeNS(attrNs, attributeName, attributeValue);
-			} else if (attributeValue !== renderElement.getAttribute(attributeName)) {
-				// Added conditional to make sure attribute value isn't the same
-				renderElement.setAttribute(attributeName, attributeValue);
-			}
-		} else {
-			if (isPresent(attrNs)) {
-				renderElement.removeAttributeNS(attrNs, attrNameWithoutNs);
-			} else {
-				renderElement.removeAttribute(attributeName);
-			}
-		}
-	}
-}
-
-export function isNamespaced(name: string) {
-	return name[0] === ':';
-}
-
-export function splitNamespace(name: string): string[] {
-	const match = name.match(NS_PREFIX_RE);
-	return [match[1], match[2]];
 }
 
 function getExistingElement(parent: Element | DocumentFragment, name: string) {
@@ -136,8 +95,4 @@ function removeUnPreservedChildren(root: Element, attr: string, isRoot?: boolean
 	}
 
 	return root;
-}
-
-export function isPresent(obj: any): boolean {
-	return obj != null;
 }
