a487bb9a4d44ea45ea929d1172c2a213c612ccad
FrozenPandaz <jasonjean1993@gmail.com>
2017-02-14 01:14:23 -0500

    use es2015 and ngc

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -12,7 +12,7 @@ import { AppModule } from './app.module';
 import { AppComponent } from './index';
 // import { RouterModule } from '@angular/router';
 // import { appRoutes } from './app/app.routing';
-import { HydrationRendererModule } from '../hydration-renderer/hydration-renderer.module';
+import { HydrationRendererModule } from 'ng-hydration-renderer';
 
 /**
  * Top-level NgModule "container"
