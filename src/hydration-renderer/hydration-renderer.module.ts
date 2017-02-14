1984226192b0f3041f3655ce2466a0891a0998b5
FrozenPandaz <jasonjean1993@gmail.com>
2017-02-13 23:04:58 -0500

    Changed dependencies

--- src/hydration-renderer/hydration-renderer.module.ts
+++ src/hydration-renderer/hydration-renderer.module.ts
@@ -1,11 +1,7 @@
 import { NgModule, RootRenderer } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HydrationRootRenderer } from './hydration-renderer';
 
 @NgModule({
-	imports: [
-		CommonModule
-	],
 	providers: [
 		{ provide: RootRenderer, useClass: HydrationRootRenderer }
 	]
