fda9d6ef034bc650292b44e3755ee0309cf3f690
FrozenPandaz <jasonjean1993@gmail.com>
2017-02-16 16:05:12 -0500

    spacing

--- src/hydration-renderer/hydration-router-outlet.ts
+++ src/hydration-renderer/hydration-router-outlet.ts
@@ -8,11 +8,13 @@ import {
 import { ActivatedRoute, RouterOutlet, RouterOutletMap } from '@angular/router';
 
 export class HydrationRouterOutlet extends RouterOutlet {
+
 	constructor(
 		parentOutletMap: RouterOutletMap, location: ViewContainerRef,
 		resolver: ComponentFactoryResolver, name: string) {
 		super(parentOutletMap, location, resolver, name);
 	}
+
 	attach(ref: ComponentRef<any>, activatedRoute: ActivatedRoute) {
 		console.log('attach');
 		super.attach(ref, activatedRoute);
