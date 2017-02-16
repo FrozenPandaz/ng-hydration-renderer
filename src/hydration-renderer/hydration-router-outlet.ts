058b9fa44191470b5128fdd5fe83ce43b030c06c
FrozenPandaz <jasonjean1993@gmail.com>
2017-02-16 15:56:02 -0500

    Add constructor

--- src/hydration-renderer/hydration-router-outlet.ts
+++ src/hydration-renderer/hydration-router-outlet.ts
@@ -1,10 +1,19 @@
 import {
 	ComponentRef,
-	Directive } from '@angular/core';
+	Directive,
+	ViewContainerRef,
+	ComponentFactoryResolver,
+	Attribute } from '@angular/core';
 
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterOutlet, RouterOutletMap } from '@angular/router';
 
 export class HydrationRouterOutlet extends RouterOutlet {
+
+	constructor(
+		private parentOutletMap: RouterOutletMap, private location: ViewContainerRef,
+		private resolver: ComponentFactoryResolver, @Attribute('name') private name: string) {
+		super(parentOutletMap, location, resolver, name);
+	}
 	attach(ref: ComponentRef<any>, activatedRoute: ActivatedRoute) {
 		console.log('attach');
 		super.attach(ref, activatedRoute);
