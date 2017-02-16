98c5c1b493a4d93c31ff2a7d8b2e1a1f89bf7a6d
FrozenPandaz <jasonjean1993@gmail.com>
2017-02-16 15:58:34 -0500

    No separate declarations

--- src/hydration-renderer/hydration-router-outlet.ts
+++ src/hydration-renderer/hydration-router-outlet.ts
@@ -8,10 +8,9 @@ import {
 import { ActivatedRoute, RouterOutlet, RouterOutletMap } from '@angular/router';
 
 export class HydrationRouterOutlet extends RouterOutlet {
-
 	constructor(
-		private parentOutletMap: RouterOutletMap, private location: ViewContainerRef,
-		private resolver: ComponentFactoryResolver, @Attribute('name') private name: string) {
+		parentOutletMap: RouterOutletMap, location: ViewContainerRef,
+		resolver: ComponentFactoryResolver, @Attribute('name') name: string) {
 		super(parentOutletMap, location, resolver, name);
 	}
 	attach(ref: ComponentRef<any>, activatedRoute: ActivatedRoute) {
